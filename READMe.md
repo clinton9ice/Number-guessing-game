9f98083ca11583da9026da60c0b3b88fc516de3f
9ice_guy <clintonufere@gmail.com>
2022-06-03 12:51:24 +0100

    Initial Commit

---
+++ READMe.md
@@ -0,0 +1,4 @@
+Number Guessing Game
+===
+This is a basic number guessing game built with just javascript as a node project. The main focus of this is to get to understand the Math.random() method, and other logics behind it.
+Try it out [here](https://replit.com/@nextolist/Number-guessing-game#index.js)
---
+++ index.js
@@ -0,0 +1,35 @@
+/*
+This project is for the sole purpose of HNG training task.
+*/ 
+
+// This would have been stored in cookies but it ain't supported on replit
+let name = prompt("Hi there, What is your name? ");
+
+function guessNumber(userGuess) {
+  // Make sure it's a number
+  userGuess = Number(userGuess)
+
+  // random value generated
+  let randNo = Math.floor(Math.random() * 2 + 1);
+
+  if (userGuess === randNo) {
+    console.log(`CONGRATULATIOS ${name} you guessed right, the answer is ${userGuess}.`);
+  }
+  else if (userGuess > randNo) {
+    console.log("OHH No your guess was greater, try a smaller number")
+    // return guessNumber(userGuess)
+  }
+  else {
+    console.log("OOPS SORRY!! try again but this time a HIGHER Number.");
+  }
+
+}
+
+if (name) {
+  console.log(`${name} welcome to our number guessing game.`);
+  let no = Number(prompt(`Please guess a number`));
+  
+  if (isNaN(no)) {
+      console.log("Sorry only numbers are allowed");
+  }else guessNumber(no);
+}
